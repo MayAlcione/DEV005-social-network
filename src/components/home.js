c9ff8f484bdf68e8dd75df7329893af5353ddc94
MayAlcione <myalcione@gmail.com>
2023-05-22 20:44:48 -0400

    Implementación de boton de dar like

--- src/components/home.js
+++ src/components/home.js
@@ -97,6 +97,11 @@ function home(navigateTo) {
         <span>${post.contenido}</span>
         <button class="eliminar" id="btn-eliminar-${idDoc}">Eliminar</button>
         <button class="editar" id="btn-editar-${idDoc}">Editar</button>
+        <button id="likeButton" class="heart-button">
+  <i class="fas fa-heart"></i>
+  <span id="likeCount">0</span>
+</button>
+
       `;
       postsContainer.appendChild(postElement);
 
@@ -148,7 +153,55 @@ function home(navigateTo) {
           .catch((error) => {
             console.error('Error al guardar cambios: ', error);
           });
-      };         
+      };    
+      let likes = [];
+const likeButton = document.getElementById('likeButton');
+const likeCount = document.getElementById('likeCount');
+
+likeButton.addEventListener('click', handleLike);
+
+function handleLike() {
+  const user = getCurrentUser(); // Obtén el identificador del usuario actual
+
+  if (likes.includes(user)) {
+    // El usuario ya ha dado "Me gusta", así que lo eliminamos
+    likes = likes.filter((likeUser) => likeUser !== user);
+  } else {
+    // El usuario no ha dado "Me gusta", así que lo agregamos
+    likes.push(user);
+  }
+
+  updateLikeButton(); // Actualiza la apariencia del botón de "Me gusta"
+  updateLikeCount(); // Actualiza el contador de "Me gusta"
+}
+
+function updateLikeButton() {
+  const heartIcon = likeButton.querySelector('i');
+
+  if (likes.includes(getCurrentUser())) {
+    // El usuario ha dado "Me gusta", cambia la apariencia del botón
+    likeButton.classList.add('liked');
+    heartIcon.classList.add('fas');
+    heartIcon.classList.remove('far');
+  } else {
+    // El usuario no ha dado "Me gusta", restablece la apariencia del botón
+    likeButton.classList.remove('liked');
+    heartIcon.classList.remove('fas');
+    heartIcon.classList.add('far');
+  }
+}
+
+function updateLikeCount() {
+  likeCount.textContent = likes.length;
+}
+
+// Función para obtener el identificador del usuario actual
+function getCurrentUser() {
+  // Implementa tu lógica para obtener el identificador del usuario actual
+  return 'usuario1';
+}
+
+     
     });
   };
 
